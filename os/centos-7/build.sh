46a4396a30ca3d09487a004d033653b809ebbe28
GDGawri <35553301+GDGawri@users.noreply.github.com>
2018-01-18 14:33:11 +0530

    Updated Build.sh
    Added growfs for root in cloudinit

--- os/centos-7/build.sh
+++ os/centos-7/build.sh
@@ -37,6 +37,9 @@ sed -i "s/name: centos/name: cloud/" $TMP_DIR/etc/cloud/cloud.cfg
 echo "sed -i \"s/- resizefs/- resolv-conf/\" $TMP_DIR/etc/cloud/cloud.cfg"
 sed -i "s/- resizefs/- resolv-conf/" $TMP_DIR/etc/cloud/cloud.cfg
 
+echo "bootcmd:" >>$TMP_DIR/etc/cloud/cloud.cfg
+echo " - xfs_growfs /" >>$TMP_DIR/etc/cloud/cloud.cfg
+
 if [ ! -d "$TMP_DIR/etc/cloud/cloud.cfg.d" ]; then
     mkdir $TMP_DIR/etc/cloud/cloud.cfg.d
 fi
